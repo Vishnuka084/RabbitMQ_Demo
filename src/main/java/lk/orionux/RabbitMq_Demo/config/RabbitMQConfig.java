a8ad8bb76bbb467e4412b43df91d83397858b517
Vishnuka yahan <vishnukayahan84@gmail.com>
2024-03-29 01:17:14 +0530

    Config  package and RabbitMQConfig class

--- src/main/java/lk/orionux/RabbitMq_Demo/config/RabbitMQConfig.java
+++ src/main/java/lk/orionux/RabbitMq_Demo/config/RabbitMQConfig.java
@@ -1,5 +1,10 @@
 package lk.orionux.RabbitMq_Demo.config;
 
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.DirectExchange;
+import org.springframework.amqp.core.Queue;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 /**
@@ -12,6 +17,24 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RabbitMQConfig {
+    public static  final String QUEUE = "rabbit_mq_queue";
+    public static  final String EXCHANGE = "rabbit_mq_exchange";
+    public static  final String ROUTING_KEY = "rabbit_mq_key";
+
+    @Bean
+    public Queue queue(){
+        return new Queue(QUEUE);
+    }
+
+    @Bean
+    public DirectExchange directExchange(){
+        return  new DirectExchange(EXCHANGE);
+    }
+
+    @Bean
+    public Binding binding(Queue queue, DirectExchange directExchange){
+        return  BindingBuilder.bind(queue).to(directExchange).with(ROUTING_KEY);
+    }
 
 
 

7c54f21ad2fd9c7cc12990d8073e227183feafe6
Vishnuka yahan <vishnukayahan84@gmail.com>
2024-03-29 00:59:28 +0530

    first commit

---
+++ src/main/java/lk/orionux/RabbitMq_Demo/config/RabbitMQConfig.java
@@ -0,0 +1,18 @@
+package lk.orionux.RabbitMq_Demo.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by Vishnuka Yahan
+ *
+ * @author : Vishnuka Yahan
+ * @data : 3/29/2024
+ * @project : RabbitMq_Demo
+ */
+
+@Configuration
+public class RabbitMQConfig {
+
+
+
+}

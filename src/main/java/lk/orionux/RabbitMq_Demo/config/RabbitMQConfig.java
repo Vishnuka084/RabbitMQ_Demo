58dedbd55c6518390a75fc8e2d8d845739599751
Vishnuka yahan <vishnukayahan84@gmail.com>
2024-03-31 22:16:00 +0530

    Add Rabbitmq Configuration

--- src/main/java/lk/orionux/RabbitMq_Demo/config/RabbitMQConfig.java
+++ src/main/java/lk/orionux/RabbitMq_Demo/config/RabbitMQConfig.java
@@ -1,9 +1,10 @@
 package lk.orionux.RabbitMq_Demo.config;
 
-import org.springframework.amqp.core.Binding;
-import org.springframework.amqp.core.BindingBuilder;
-import org.springframework.amqp.core.DirectExchange;
-import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.*;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -17,6 +18,7 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RabbitMQConfig {
+
     public static final String QUEUE  = "rabbit_mq_queue";
     public static final String EXCHANGE  = "rabbit_mq_exchange";
     public static final String ROUTING_KEY  = "rabbit_mq_r_key";
@@ -30,12 +32,19 @@ public class RabbitMQConfig {
     public DirectExchange directExchange(){
         return new DirectExchange(EXCHANGE);
     }
-
     @Bean
     public Binding binding(Queue queue, DirectExchange directExchange){
         return  BindingBuilder.bind(queue).to(directExchange).with(ROUTING_KEY);
     }
 
+    @Bean
+    public MessageConverter messageConverter(){
+        return new Jackson2JsonMessageConverter();    }
 
-
+    @Bean
+    public AmqpTemplate getTemplate(ConnectionFactory connectionFactory){
+        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+        rabbitTemplate.setMessageConverter(messageConverter());
+        return rabbitTemplate;
+    }
 }
--- src/main/java/lk/orionux/RabbitMq_Demo/producer/RMQProducer.java
+++ src/main/java/lk/orionux/RabbitMq_Demo/producer/RMQProducer.java
@@ -25,11 +25,13 @@ public class RMQProducer {
     @Autowired
     private RabbitTemplate rabbitTemplate;
     @PostMapping("/order")
-    public OrderDTO placeOrder(@RequestBody Order order){
+    public  OrderDTO placeOrder(@RequestBody Order order){
 
         OrderDTO orderDTO = new OrderDTO(order,"Order Placed" , "Hi Producer Your Order is Placed ");
 
         rabbitTemplate.convertAndSend(RabbitMQConfig.EXCHANGE, RabbitMQConfig.ROUTING_KEY , orderDTO);
         return orderDTO;
+
+
     }
 }
